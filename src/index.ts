55a39c6a37ecfc2c24c07741d6b64513d76bb881
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-24 09:06:16 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { resolveClaudiaShareable } from './resolveClaudiaShareable';
+export { default as ClaudiaShareableManager } from './ClaudiaShareableManager';
+export type { ClaudiaShareableManagerProps, ClaudiaShareableManagerCopy } from './ClaudiaShareableManager';
+export type { ClaudiaShareable, ClaudiaShareableResolution } from './types';
